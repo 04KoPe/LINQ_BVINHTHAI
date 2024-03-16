12ab2086901e661b3789d3d2ee1096f89f2a970e
04KoPe <vinhthai2905@aol.com>
2024-03-16 21:56:46 +0700

    Add project files.

---
+++ LINQ_BVINHTHAI/Khoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_BVINHTHAI
+{
+    class Khoa
+    {
+        public string tenKhoa { get; set; }
+        public List<SinhVien> DanhSachSinhVien { get; set; }
+    }
+}
---
+++ LINQ_BVINHTHAI/SinhVien.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_BVINHTHAI
+{
+    class SinhVien
+    {
+
+        public string tenSinhVien { get; set; }
+        public double diemTrungBinh { get; set; }
+    }
+}
